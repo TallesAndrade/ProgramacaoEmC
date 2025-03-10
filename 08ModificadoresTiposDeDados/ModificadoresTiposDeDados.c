23c7472d96632af6b4b81e1e2eed453fbb57e198
Talles <tallesandrade7@gmail.com>
2025-03-10 20:58:01 -0300

    aula modificadores tipos de dados

--- 08ModificadoresTiposDeDados/ModificadoresTiposDeDados.c
+++ 08ModificadoresTiposDeDados/ModificadoresTiposDeDados.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int main(){
+    //Modificador unsigned
     int numeroSinal = 3000000000; //Este valor excede o limite de um int nomal
     unsigned int numeroSemSinal = 3000000000;
 
@@ -9,6 +10,7 @@ int main(){
 
     printf("--------------------------------\n");
 
+    //Modificador long
     int numeroNormal = 2147483647;//Valor maximo int
     long int numeroGrande = 2147483647;
 
@@ -19,7 +21,22 @@ int main(){
 
     printf("Número grande atualizado (long int) %ld\n ",numeroGrande);
 
+    printf("--------------------------------\n");
+    //bytes de cada tipo de dado
+    printf("Tamanho de int: %u bytes\n", sizeof(int));
+    printf("Tamanho de long int: %u bytes\n", sizeof(long int));
+    printf("Tamanho de long long int: %u bytes\n", sizeof(long long int));
+    printf("Tamanho de double: %u bytes\n", sizeof(double));
+    printf("Tamanho de long double: %u bytes\n", sizeof(long double));
 
+    printf("--------------------------------\n");
+
+
+    double numeroPreciso = 3.141592653589793;
+    long double numeroMuitoPreciso = 3.14159265358979323846;
+ 
+    printf("Número preciso (double): %.15u\n", numeroPreciso);
+    printf("Número muito preciso (long double): %.21Lu\n", numeroMuitoPreciso);
 
     return 0;
 }
