608aab9529d230b8d0fc75c65ed6b65d31d310c0
Talles <tallesandrade7@gmail.com>
2025-03-10 21:27:47 -0300

    .

--- 09ComparacaoEstoque/ComparacaoEstoque.c
+++ 09ComparacaoEstoque/ComparacaoEstoque.c
@@ -2,28 +2,30 @@
 
 int main() {
     // Dados do Produto 1
-    unsigned long long quantidade1 = 3000000000ULL;  
-    long long preco1 = 100000LL;
-    unsigned long long valorTotal1 = quantidade1 * preco1;
+    unsigned long long estoque1 = 10000;  // Exemplo de estoque
+    long long preco1 = 299;              // Preço unitário
+    unsigned long long valorTotal1 = estoque1 * preco1;
 
     // Dados do Produto 2
-    unsigned long long quantidade2 = 4000000000ULL;  
-    long long preco2 = 50000LL;
-    unsigned long long valorTotal2 = quantidade2 * preco2;
+    unsigned long long estoque2 = 12000;
+    long long preco2 = 399;
+    unsigned long long valorTotal2 = estoque2 * preco2;
 
+    // Exibe as informações dos produtos
     printf("=== PRODUTO 1 ===\n");
-    printf("Quantidade: %llu\n", quantidade1);
-    printf("Preço (unidade): %lld\n", preco1);
-    printf("Valor Total: %llu\n\n", valorTotal1);
+    printf("Estoque: %llu\n", estoque1);
+    printf("Preço (unidade): R$ %lld\n", preco1);
+    printf("Valor Total: R$ %llu\n\n", valorTotal1);
 
     printf("=== PRODUTO 2 ===\n");
-    printf("Quantidade: %llu\n", quantidade2);
-    printf("Preço (unidade): %lld\n", preco2);
-    printf("Valor Total: %llu\n\n", valorTotal2);
+    printf("Estoque: %llu\n", estoque2);
+    printf("Preço (unidade): R$ %lld\n", preco2);
+    printf("Valor Total: R$ %llu\n\n", valorTotal2);
 
-    printf("Produto 1 tem maior quantidade que Produto 2? %d\n", quantidade1 > quantidade2);
-    printf("Produto 1 tem menor valor total que Produto 2? %d\n", valorTotal1 < valorTotal2);
-    printf("Produto 1 e Produto 2 têm o mesmo preço? %d\n", preco1 == preco2);
+    // Comparações (sem estruturas de controle)
+    printf("Produto 1 tem maior estoque que Produto 2? %d\n", (estoque1 > estoque2));
+    printf("Produto 1 tem menor valor total que Produto 2? %d\n", (valorTotal1 < valorTotal2));
+    printf("Produto 1 e Produto 2 têm o mesmo preço? %d\n", (preco1 == preco2));
 
     return 0;
-}
+
