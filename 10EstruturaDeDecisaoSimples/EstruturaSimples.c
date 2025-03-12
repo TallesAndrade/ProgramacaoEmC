257367df7113a84524115cc574781dbc57d4fce2
Talles <tallesandrade7@gmail.com>
2025-03-12 18:34:16 -0300

    aulas Estruturas de decisao simples e composta

---
+++ 10EstruturaDeDecisaoSimples/EstruturaSimples.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+int main(){
+    int numero1, numero2;
+
+    numero1 = 10;
+    numero2 = 5;
+
+    if (numero1 > numero2){
+        printf("Número 1 é maior que número 2\n");
+
+    }
+    
+    printf("Fora do if\n"); 
+
+    printf("---------------------\n");
+
+    int idade = 20;
+    if (idade >= 18){
+        printf("Você é maior de idade\n");
+        printf("Dentro do if\n");
+    }
+    printf("Fora do if\n");
+
+    printf("---------------------\n");
+
+    float temperatura = 32.7;
+
+    if (temperatura > 30){
+        printf("Está fazendo calor");
+    }
+
+    printf("---------------------\n");
+
+    int nota = 75;
+
+    if (nota >= 60) {
+    printf("Você passou!\n");
+    }
+
+    printf("---------------------\n");
+
+
+    int numero = 4;
+
+    if (numero % 2 == 0) {
+    printf("O número é par\n");
+    }
+
+    printf("---------------------\n");
+
+
+    int estoque = 3;
+
+    if (estoque < 5) {
+    printf("Estoque baixo\n");
+    }
+
+
+    
+    return 0;
+}
---
+++ 11EstruturaDeDecisaoComposta/EstruturaComposta.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 10;
+
+    if (numero % 2 == 0)
+    {
+        printf("O número é par\n");
+    }
+    else
+    {
+        printf("O número é ímpar\n");
+    }
+
+    printf("---------------------\n");
+
+    float temperatura = 25.0;
+
+    if (temperatura > 30.0)
+    {
+        printf("Está calor\n");
+    }
+    else
+    {
+        printf("Não está calor\n");
+    }
+
+    printf("---------------------\n");
+
+    int nota = 65;
+
+    if (nota >= 60)
+    {
+        printf("Você passou!\n");
+    }
+    else
+    {
+        printf("Você não passou.\n");
+    }
+
+    printf("---------------------\n");
+
+    int idade = 16;
+
+    if (idade >= 18)
+    {
+        printf("Você é maior de idade\n");
+    }
+    else
+    {
+        printf("Você é menor de idade\n");
+    }
+
+    printf("---------------------\n");
+
+    float preco = 120.0;
+
+    if (preco > 100.0)
+    {
+        printf("O produto é caro\n");
+    }
+    else
+    {
+        printf("O produto é barato\n");
+    }
+
+    printf("---------------------\n");
+
+    int estoque = 3;
+
+    if (estoque < 5)
+    {
+        printf("Estoque baixo\n");
+    }
+    else
+    {
+        printf("Estoque adequado\n");
+    }
+
+    return 0;
+}
